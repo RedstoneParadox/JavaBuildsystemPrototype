c05a718d61ca4d080b5f8d34ca4771560d7a389f
RedstoneParadox <spinjitzu00@gmail.com>
2021-11-01 20:23:01 -0700

    Update BuildscriptProvider.java

--- src/main/java/io/github/redstoneparadox/buildsystem/api/buildscript/BuildscriptProvider.java
+++ src/main/java/io/github/redstoneparadox/buildsystem/api/buildscript/BuildscriptProvider.java
@@ -1,4 +1,23 @@
 package io.github.redstoneparadox.buildsystem.api.buildscript;
 
+import io.github.redstoneparadox.buildsystem.sources.SourceSet;
+
+import java.nio.file.Path;
+import java.util.Collection;
+
 public interface BuildscriptProvider {
+	/**
+	 * Runs the buildscript for the project.
+	 *
+	 * @param path Path to the project directory
+	 */
+	void run(Path path);
+
+	/**
+	 * Gets all the source sets
+	 *
+	 * @return A collection of all source sets
+	 * specified by the build script.
+	 */
+	Collection<SourceSet> getSourceSets();
 }
