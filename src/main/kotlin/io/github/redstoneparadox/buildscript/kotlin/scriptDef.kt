acb10e2feae56fa6a9d7773e44212850ca1798d2
RedstoneParadox <spinjitzu00@gmail.com>
2022-01-31 20:27:15 -0800

    Create scriptDef.kt

---
+++ src/main/kotlin/io/github/redstoneparadox/buildscript/kotlin/scriptDef.kt
@@ -0,0 +1,40 @@
+package io.github.redstoneparadox.buildscript.kotlin
+
+import kotlinx.coroutines.runBlocking
+import kotlin.script.experimental.annotations.KotlinScript
+import kotlin.script.experimental.api.*
+import kotlin.script.experimental.dependencies.*
+import kotlin.script.experimental.dependencies.maven.MavenDependenciesResolver
+import kotlin.script.experimental.jvm.JvmDependency
+import kotlin.script.experimental.jvm.dependenciesFromCurrentContext
+import kotlin.script.experimental.jvm.jvm
+
+@KotlinScript(
+    fileExtension = "buildscript.kts",
+    compilationConfiguration = BuildscriptDefConfiguration::class
+)
+abstract class BuildscriptDef
+
+object BuildscriptDefConfiguration: ScriptCompilationConfiguration({
+    defaultImports(DependsOn::class, Repository::class)
+    jvm {
+        dependenciesFromCurrentContext(wholeClasspath = true)
+    }
+    refineConfiguration {
+        onAnnotations(DependsOn::class, Repository::class, handler = ::handleBuildscript)
+    }
+})
+
+private val resolver = CompoundDependenciesResolver(FileSystemDependenciesResolver(), MavenDependenciesResolver())
+
+fun handleBuildscript(context: ScriptConfigurationRefinementContext): ResultWithDiagnostics<ScriptCompilationConfiguration> {
+    val annotations = context.collectedData?.get(ScriptCollectedData.collectedAnnotations)?.takeIf { it.isNotEmpty() }
+        ?: return context.compilationConfiguration.asSuccess()
+    return runBlocking {
+        resolver.resolveFromScriptSourceAnnotations(annotations)
+    }.onSuccess {
+        context.compilationConfiguration.with {
+            dependencies.append(JvmDependency(it))
+        }.asSuccess()
+    }
+}
